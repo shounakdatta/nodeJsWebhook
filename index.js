a3d4a237fff28f56877090cdd0a38c8c54d57705
shounakdatta <33079683+shounakdatta@users.noreply.github.com>
2018-04-03 11:04:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&
